43f627c8bc0d781a16c9141a6520e8b880bf16fa
deShalom <jordanunderwoodisepic@gmail.com>
2020-05-23 01:00:20 +0100

    Class to functionality division.
    Class to functionality division.

---
+++ Voice Test/Assets/Scripts/QuizManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    public GameObject[] Quizes;
+
+    public void nextQuiz()
+    {
+        int rando = Random.Range(1, Quizes.Length);
+        Quizes[rando].SetActive(true);
+    }
+
+}
---
+++ Voice Test/Assets/Scripts/VoiceRec.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+using System;
+using Random = UnityEngine.Random;
+
+public class VoiceRec : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    GameObject currentQuiz;
+    string quizName;
+    public GameObject gameMan;
+
+    private void Start()
+    {
+        actions.Add("sparrow hawk", hawkSaid);
+        actions.Add("cuckoo", hawkSaid);
+        actions.Add("robin", hawkSaid);
+        actions.Add("peregrine falcon", hawkSaid);
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += recognisedWord;
+        keywordRecognizer.Start();
+        gameMan.GetComponent<QuizManager>().nextQuiz();
+    }
+
+    void hawkSaid()
+    {
+        print("Fly you fools");
+    }
+
+    void recognisedWord(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        quizCheck();
+
+        if (speech.text == quizName)
+        {
+            actions[speech.text].Invoke();
+            currentQuiz.SetActive(false);
+            gameMan.GetComponent<QuizManager>().nextQuiz();
+        }
+
+    }
+
+    void quizCheck()
+    {
+        currentQuiz = GameObject.FindWithTag("Quiz");
+        quizName = currentQuiz.name;
+    }
+
+}
